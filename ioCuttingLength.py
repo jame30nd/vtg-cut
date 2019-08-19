290466a66d89c09a08061d7c6dd9fbdca5d36434
Jamebond <tanakorn.niamchaona@gmail.com>
2019-08-19 10:12:42 +0700

    update

--- ioCuttingLength.py
+++ ioCuttingLength.py
@@ -3,7 +3,19 @@ import csv
 import pandas
 reader = csv.reader(
     open("csvfile.csv"), delimiter=";")
-
-
+included_cols = [22]
+# print(reader)
+count = 0
+content = []
 for row in reader:
-    print(row[0])
+    # content = list(row[i] for i in included_cols)
+    content.append(row[22])
+    # print()
+    #
+num_list = len(content)
+num_list_target = num_list - 1
+print(content[num_list_target-1])
+# num = content.index()
+# if count == 12:
+#     print(row[22])
+# count += 1
